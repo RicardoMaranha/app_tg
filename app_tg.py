a07df49192b7bc05fbebab8523b67f122d1bb419
RicardoMaranha <ricardomaranhapro@outlook.com>
2023-09-04 10:42:13 -0300

    primeirocommit

--- app_tg.py
+++ app_tg.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 
-
-app= Flask(__name__)
+app = Flask(__name__)
 
 
 class Cliente:
@@ -27,23 +26,29 @@ class Fornecedor:
         self.email = email
         self.id_fornecedor = id_fornecedor
 
+
 lista_fornecedor = []
 
 
 @app.route('/inicio')
 def inicio():
-    fornecedor1 = Fornecedor('11111111001/0001','Fabrica de Borracha','Rua Amazonas', '16993933477','fb@gmail.com', '1')
-    fornecedor2 = Fornecedor('11111111001/0001','Fabrica de Borracha','Rua Amazonas', '16993933477','fb@gmail.com', '1')
-    lista_fornecedor=[fornecedor1, fornecedor2]
-    siteNome= "Home"
-    return render_template('index.html',  fornecedores = lista_fornecedor)
+    fornecedor1 = Fornecedor('11111111001/0001', 'Fabrica de Borracha', 'Rua Amazonas', '16993933477', 'fb@gmail.com',
+                             '1')
+    fornecedor2 = Fornecedor('11111111001/0001', 'Fabrica de Borracha', 'Rua Amazonas', '16993933477', 'fb@gmail.com',
+                             '1')
+    lista_fornecedor = [fornecedor1, fornecedor2]
+    siteNome = "Home"
+    return render_template('index.html', fornecedores=lista_fornecedor)
+
 
 @app.route('/teste')
 def teste():
-    fornecedor1 = Fornecedor('11111111001/0001','Fabrica de Borracha','Rua Amazonas', '16993933477','fb@gmail.com', '1')
-    fornecedor2 = Fornecedor('11111111001/0001','Fabrica de Borracha','Rua Amazonas', '16993933477','fb@gmail.com', '1')
-    lista_fornecedor=[fornecedor1, fornecedor2]
-    return render_template('listaFornecedor.html', fornecedores = lista_fornecedor)
+    fornecedor1 = Fornecedor('11111111001/0001', 'Fabrica de Borracha', 'Rua Amazonas', '16993933477', 'fb@gmail.com',
+                             '1')
+    fornecedor2 = Fornecedor('11111111001/0001', 'Fabrica de Borracha', 'Rua Amazonas', '16993933477', 'fb@gmail.com',
+                             '1')
+    lista_fornecedor = [fornecedor1, fornecedor2]
+    return render_template('listaFornecedor.html', fornecedores=lista_fornecedor)
+
 
 app.run(debug=True)
-
