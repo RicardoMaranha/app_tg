fc3680a73644e93c42abcf19549d9a8b8c2995de
RicardoMaranha <ricardomaranhapro@outlook.com>
2023-09-07 19:52:41 -0300

    front1

--- app_tg.py
+++ app_tg.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, flash
 
 app = Flask(__name__)
+app.secret_key = 'cecilia'
 
 
 class Cliente:
@@ -18,37 +19,69 @@ class Cliente:
 
 
 class Fornecedor:
-    def __init__(self, cnpj, nome, endereco, telefone, email, id_fornecedor):
+    def __init__(self, cnpj, nome, endereco, telefone, email):
         self.cnpj = cnpj
         self.nome = nome
         self.endereco = endereco
         self.telefone = telefone
         self.email = email
-        self.id_fornecedor = id_fornecedor
+     
+
+class Usuario:
+    def __init__(self,nome,nickname, senha):
+        self.nome = nome
+        self.nickname = nickname
+        self.senha = senha
 
 
+usuario1 = Usuario("Ricardo","rickmaranha", "maranha" )
+usuario2 = Usuario("Cecília", "ceciliamaranha", "mariaclara")
 lista_fornecedor = []
 
 
-@app.route('/inicio')
+@app.route('/')
 def inicio():
-    fornecedor1 = Fornecedor('11111111001/0001', 'Fabrica de Borracha', 'Rua Amazonas', '16993933477', 'fb@gmail.com',
-                             '1')
-    fornecedor2 = Fornecedor('11111111001/0001', 'Fabrica de Borracha', 'Rua Amazonas', '16993933477', 'fb@gmail.com',
-                             '1')
-    lista_fornecedor = [fornecedor1, fornecedor2]
-    siteNome = "Home"
-    return render_template('index.html', fornecedores=lista_fornecedor)
+    return render_template('index.html')
 
+@app.route('/listaFornecedor')
+def listaFornecedor():
+    return render_template('listaFornecedor.html', fornecedores = lista_fornecedor)
 
-@app.route('/teste')
-def teste():
-    fornecedor1 = Fornecedor('11111111001/0001', 'Fabrica de Borracha', 'Rua Amazonas', '16993933477', 'fb@gmail.com',
-                             '1')
-    fornecedor2 = Fornecedor('11111111001/0001', 'Fabrica de Borracha', 'Rua Amazonas', '16993933477', 'fb@gmail.com',
-                             '1')
-    lista_fornecedor = [fornecedor1, fornecedor2]
-    return render_template('listaFornecedor.html', fornecedores=lista_fornecedor)
+@app.route('/cadastraFornecedor')
+def cadastraFornecedor():
+    return render_template('cadastraFornecedor.html')
+
+@app.route('/criarFornecedor', methods=['POST',])
+def criarFornecedor():
+    cnpj = request.form['cnpj']
+    nome = request.form['nome']
+    endereco = request.form['endereco']
+    telefone = request.form['telefone']
+    email = request.form['email']
+    fornecedor = Fornecedor(cnpj,nome,endereco,telefone,email)
+    lista_fornecedor.append(fornecedor)
+    return redirect('/listaFornecedor')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'admin' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + 'Usuário logado com sucesso')
+        return redirect('/')
+    else:
+        flash('Usuario não logado')
+        return redirect('/login')
+    
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect('/') 
 
 
 app.run(debug=True)
